7dbf902683ea2d78aee8fcaa8d2f11ed0c37b559
kiddieverts <kiddieverts@gmail.com>
2022-05-10 14:07:53 +0000

    Fix bug

--- index.js
+++ index.js
@@ -4,36 +4,33 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const getDetailsHtml = (httpResponse) => {
-  const htmlData = httpResponse.data;
-  return htmlData;
-}
-
 const getOrigin = (htmlData) => {
   const arr = htmlData.split('</tr><tr><th scope="row" class="infobox-label">Origin</th>');
-  if (arr.length === 0) {
-    return 'finnst ekki'
+  if (arr.length < 2) {
+    return 'Finnst ekki';
   }
-  const end = arr[1];
-  const ending = end.split('</td>');
-  const x = ending[0];
-  const regex = /<[^>]*>/gi
-  const country = x.replace(regex, '');
+  const a = arr[1];
+  const b = a.split('</td>');
+  const inside = b[0];
+
+  const regex = /<[^>]*>/gi;
+  const country = inside.replace(regex, '');
   return country;
 }
 
 app.get('/', (request, response) => {
-  const artist = request.query.q;
-  const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${artist}&format=json`
+  const artist = encodeURI(request.query.q);
+  const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${artist}&format=json`;
 
   axios.get(url).then(result => {
     const detailsUrl = result.data[3][0];
     axios.get(detailsUrl)
-      .then(getDetailsHtml)
+      .then(response => response.data)
       .then(htmlData => getOrigin(htmlData))
       .then(county => response.send(county));
   });
 });
 
 app.listen(7070);
+console.log('Running...');
 
