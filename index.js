da48e3639a76967c9af696e40e7e5a46fa9863b2
Ísak <jonsson.isak99@gmail.com>
2022-05-10 12:36:51 +0000

    i vinnslu

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// https://en.wikipedia.org/w/index.php?search=The+Lathums&title=Special:Search&profile=advanced&fulltext=1&ns0=1
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/', (request, response) => {
+    const artist = 'The Lathums';
+    const url = `https://en.wikipedia.org/w/index.php?search=${artist}&title=Special:Search&profile=advanced&fulltext=1&ns0=1`;
+    axios.get(url).then(result => {
+        console.log(result);
+        response.send(result.data);
+    })
+    console.log('abba');
+});
+
+app.listen(5000);
+
