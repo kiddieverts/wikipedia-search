1b528e6ff79eb2f60441ad86f92220c48b441f96
kiddieverts <kiddieverts@gmail.com>
2022-05-10 13:04:58 +0000

    almost ready

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+// const url = `https://en.wikipedia.org/w/index.php?search=${artist}&title=Special:Search&profile=advanced&fulltext=1&ns0=1`;
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
+const getDetailsHtml = (httpResponse) => {
+  const htmlData = httpResponse.data;
+  return htmlData;
+}
+
+const getOrigin = (htmlData) => {
+  const arr = htmlData.split('</tr><tr><th scope="row" class="infobox-label">Origin</th>');
+  const end = arr[1];
+
+  return end;
+}
+
 app.get('/', (request, response) => {
-    const artist = 'The Lathums';
-    const url = `https://en.wikipedia.org/w/index.php?search=${artist}&title=Special:Search&profile=advanced&fulltext=1&ns0=1`;
-    axios.get(url).then(result => {
-        console.log(result);
-        response.send(result.data);
-    })
-    console.log('abba');
+  const artist = 'The Lathums';
+  const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${artist}&format=json`
+
+  axios.get(url).then(result => {
+    const detailsUrl = result.data[3][0];
+    axios.get(detailsUrl)
+      .then(getDetailsHtml)
+      .then(htmlData => getOrigin(htmlData))
+      .then(county => response.send(county));
+  });
 });
 
-app.listen(5000);
+app.listen(7070);
 
